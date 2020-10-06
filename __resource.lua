9a6e7c0fa4aca6694fe2d4ce79075673b4d8909b
Elior <50843891+Elior2739@users.noreply.github.com>
2020-10-06 10:01:06 +0300

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,9 +1,8 @@
--- © This Script Coded By Mr.Itchy#2223 And Elior#0590 © --
+-- © This Resource Coded By Elior#0590 And Mr.Itchy#2223 © --
+-- © This Resource Protected By Apache-2.0 License © --
 
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-game 'gta5'
+server_script 'server.lua'
 
-server_scripts {
-	'server.lua'
-}
+dependency 'es_extended'
